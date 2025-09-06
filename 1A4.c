bf87c4010dbe6a0d604d1408d2d7622f6be9c956
Jash Savsani <savsanijash8@gmail.com>
2025-09-06 20:17:33 +0530

    Add files via upload

---
+++ 1A4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter First Number:");
+	scanf("%d",&a);
+	printf("Enter Second Number:");
+	scanf("%d",&b);
+	printf("Enter Third Number:");
+	scanf("%d",&c);
+	if(a>=b && a>=c){
+		printf("%d is Largest.",a);
+	}
+	if(b>=c){
+		printf("%d is Largest.",b);
+	}
+	else{
+		printf("%d is Largest.",c);
+	}
+}
